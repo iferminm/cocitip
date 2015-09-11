8ae2fc66184a6a228959a0ebeef5dea92117250f
Israel Fermin Montilla <iferminm@gmail.com>
2015-09-11 14:11:25 +0400

    Migrated to flask, app skeleton

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/recipe/<uuid>')
+def recipe(uuid):
+    return render_template('recipe.html', recipe_id=uuid)
+
+
+@app.route('/recipes/')
+def recipes():
+    return 'This is the recipes page'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
